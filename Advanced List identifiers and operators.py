9d3c1a3722e634b9bacc4bd032df1127d0950691
Joshua D Smith <2smithjosh@gmail.com>
2024-10-11 12:27:19 -0700

    Python Lists questions 1-3

---
+++ Advanced List identifiers and operators.py
@@ -0,0 +1,9 @@
+#task 1
+
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+if "Alice" in submitted and "Alice" in attended:
+    print("Alice is in both lists")
+else:
+    print("Alice is not in both lists")
---
+++ Python List Transformation.py
@@ -0,0 +1,10 @@
+#task 1 sort from highest to lowest
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort(reverse = True)
+
+print(grades)
+
+#task 2 find the average
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+average = sum(grades) / len(grades)
+print(average)
